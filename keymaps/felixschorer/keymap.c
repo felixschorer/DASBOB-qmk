eb6a0bad0d440a734065c3157837311b00af2235
felixschorer <felixschorer@users.noreply.github.com>
2025-08-17 18:00:01 +0200

    Reorganize nav and mods

--- keymaps/felixschorer/keymap.c
+++ keymaps/felixschorer/keymap.c
@@ -35,21 +35,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [SYM] = LAYOUT_split_3x5_3(
         KC_TILD, KC_LBRC, KC_LCBR, KC_LPRN, KC_CIRC, KC_DLR,  KC_RPRN, KC_RCBR, KC_RBRC, KC_GRV,
-        KC_HASH, KC_AMPR, KC_PLUS, KC_ASTR, KC_EQL,  KC_UNDS, CM_LCTL, TD_ALT,  CM_LGUI, CM_LSFT,
+        KC_HASH, KC_AMPR, KC_PLUS, KC_ASTR, KC_EQL,  KC_UNDS, CM_LSFT, CM_LCTL, TD_ALT,  CM_LGUI,
         XXXXXXX, KC_PIPE, KC_MINS, KC_SLSH, KC_PERC, KC_AT,   KC_BSLS, KC_EXLM, KC_QUES, XXXXXXX,
                           _______, _______, _______, _______, _______, _______
     ),
 
     [NAV] = LAYOUT_split_3x5_3(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ESC,  KC_INS,  XXXXXXX, XXXXXXX, XXXXXXX,
-        CM_LSFT, CM_LGUI, TD_ALT,  CM_LCTL, XXXXXXX, KC_BSPC, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT,
-        AU_TOGG, XXXXXXX, XXXXXXX, KC_PSCR, XXXXXXX, KC_DEL,  KC_HOME, KC_PGUP, KC_PGDN, KC_END,
+        KC_ESC,  XXXXXXX, XXXXXXX, KC_PSCR, XXXXXXX, KC_DEL,  KC_HOME, KC_UP,   KC_END,  XXXXXXX,
+        CM_LGUI, TD_ALT,  CM_LCTL, CM_LSFT, XXXXXXX, KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,
+        AU_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  KC_PGUP, XXXXXXX, KC_PGDN, XXXXXXX,
                           _______, _______, _______, _______, _______, _______
     ),
 
     [NUM] = LAYOUT_split_3x5_3(
         KC_9,    KC_7,    KC_5,    KC_3,    KC_1,    KC_0,    KC_2,    KC_4,    KC_6,    KC_8,
-        CM_LSFT, CM_LGUI, TD_ALT,  CM_LCTL, KC_F11,  KC_F12,  CM_LCTL, TD_ALT,  CM_LGUI, CM_LSFT,
+        CM_LGUI, TD_ALT,  CM_LCTL, CM_LSFT, KC_F11,  KC_F12,  CM_LSFT, CM_LCTL, TD_ALT,  CM_LGUI,
         KC_F9,   KC_F7,   KC_F5,   KC_F3,   KC_F1,   KC_F10,  KC_F2,   KC_F4,   KC_F6,   KC_F8,
                           _______, _______, _______, _______, _______, _______
     ),
