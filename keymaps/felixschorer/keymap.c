dc6a0455d3f2599a190eef4b2a283200487d9692
felixschorer <felixschorer@users.noreply.github.com>
2025-11-15 19:12:31 +0100

    Disable oneshot modifiers

--- keymaps/felixschorer/keymap.c
+++ keymaps/felixschorer/keymap.c
@@ -35,21 +35,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [SYM] = LAYOUT_split_3x5_3(
         KC_TILD, KC_LBRC, KC_LCBR, KC_LPRN, KC_CIRC, KC_DLR,  KC_RPRN, KC_RCBR, KC_RBRC, KC_GRV,
-        KC_HASH, KC_AMPR, KC_PLUS, KC_ASTR, KC_EQL,  KC_UNDS, CM_LSFT, CM_LCTL, TD_ALT,  CM_LGUI,
+        KC_HASH, KC_AMPR, KC_PLUS, KC_ASTR, KC_EQL,  KC_UNDS, KC_LSFT, KC_LCTL, TD_ALT,  KC_LGUI,
         XXXXXXX, KC_PIPE, KC_MINS, KC_SLSH, KC_PERC, KC_AT,   KC_BSLS, KC_QUES, KC_EXLM, XXXXXXX,
                           _______, _______, _______, _______, _______, _______
     ),
 
     [NAV] = LAYOUT_split_3x5_3(
         KC_ESC,  XXXXXXX, XXXXXXX, KC_PSCR, XXXXXXX, XXXXXXX, KC_BSPC, KC_DEL,  XXXXXXX, KC_INS,
-        CM_LGUI, TD_ALT,  CM_LCTL, CM_LSFT, XXXXXXX, XXXXXXX, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT,
+        KC_LGUI, TD_ALT,  KC_LCTL, KC_LSFT, XXXXXXX, XXXXXXX, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT,
         AU_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN, KC_END,
                           _______, _______, _______, _______, _______, _______
     ),
 
     [NUM] = LAYOUT_split_3x5_3(
         KC_9,    KC_7,    KC_5,    KC_3,    KC_1,    KC_0,    KC_2,    KC_4,    KC_6,    KC_8,
-        CM_LGUI, TD_ALT,  CM_LCTL, CM_LSFT, KC_F11,  KC_F12,  CM_LSFT, CM_LCTL, TD_ALT,  CM_LGUI,
+        KC_LGUI, TD_ALT,  KC_LCTL, KC_LSFT, KC_F11,  KC_F12,  KC_LSFT, KC_LCTL, TD_ALT,  KC_LGUI,
         KC_F9,   KC_F7,   KC_F5,   KC_F3,   KC_F1,   KC_F10,  KC_F2,   KC_F4,   KC_F6,   KC_F8,
                           _______, _______, _______, _______, _______, _______
     ),
@@ -79,10 +79,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     update_oneshot(&cm_lgui, keycode, key_down);
     update_oneshot(&cm_lsft, keycode, key_down);
 
-    if (keycode == TD_ALT && !key_down) {
-        release_oneshot(&cm_lalt);
-        release_oneshot(&cm_ralt);
-    }
+    //if (keycode == TD_ALT && !key_down) {
+    //    release_oneshot(&cm_lalt);
+    //    release_oneshot(&cm_ralt);
+    //}
 
     return true;
 }
@@ -106,7 +106,8 @@ void handle_alt_tapdance(tap_dance_state_t *state, void *user_data) {
 }
 
 tap_dance_action_t tap_dance_actions[] = {
-    [LALT_RALT] = ACTION_TAP_DANCE_FN(handle_alt_tapdance),
+    //[LALT_RALT] = ACTION_TAP_DANCE_FN(handle_alt_tapdance),
+    [LALT_RALT] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_RALT),
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
